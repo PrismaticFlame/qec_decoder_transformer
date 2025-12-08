88179f38455a2611bf75b429cbc5207163a75b1a
Arjun <arjuns8@illinois.edu>
2025-12-08 15:02:54 -0500

    Train and Prediction function and pipeline file to run simulted stim data on transformer and MWPM

--- src/qec/train_transformer.py
+++ src/qec/train_transformer.py
@@ -6,8 +6,8 @@ from torch.utils.data import DataLoader, random_split
 from datetime import datetime, timedelta
 import time
 
-from transformer_decoder.dataset import SyndromeDataset
-from transformer_decoder.model import QECAlphaTransformer
+from qec.transformer_decoder.dataset import SyndromeDataset
+from qec.transformer_decoder.model import QECAlphaTransformer
 
 def format_seconds(s):
     """Convert seconds to H:MM:SS format."""
@@ -26,23 +26,23 @@ def compute_ler_from_logits(logits, labels):
         ler = errors.mean().item()
     return ler
 
-
-def main():
+def train(data, logs, measurement=False):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using device:", device)
 
-    # 1. Load Stim-generated dataset
-    dets = np.loadtxt("stim_files/data/detector_samples.csv",
-                      delimiter=",", dtype=np.uint8)
+    # # 1. Load Stim-generated dataset
+    # dets = np.loadtxt("stim_files/data/detector_samples.csv",
+    #                   delimiter=",", dtype=np.uint8)
     
-    soft_measurements = np.loadtxt("stim_files/data/gausian_soft_mesurements.csv",
-                      delimiter=",", dtype=np.float32)
-    logs = np.loadtxt("stim_files/data/logical_labels.csv",
-                      delimiter=",", dtype=np.uint8)
+    # soft_measurements = np.loadtxt("stim_files/data/gausian_soft_mesurements.csv",
+    #                   delimiter=",", dtype=np.float32)
+    # logs = np.loadtxt("stim_files/data/logical_labels.csv",
+    #                   delimiter=",", dtype=np.uint8)
 
     rounds = 5  # MUST match your Stim gen_dem() parameter
 
-    full_dataset = SyndromeDataset(dets, logs, distance=3, rounds=rounds, measurement=False)
+    full_dataset = SyndromeDataset(data, logs, distance=3, rounds=rounds, measurement=measurement)
+    # full_dataset = SyndromeDataset(dets, logs, distance=3, rounds=rounds, measurement=False)
     # full_dataset = SyndromeDataset(soft_measurements, logs, distance=3, rounds=rounds, measurement=True)
     N = len(full_dataset)
     print(f"Total shots: {N}, num_detectors: {full_dataset.num_detectors}, "
@@ -78,6 +78,8 @@ def main():
     print(f"Training started at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
 
     # 4. Training loop
+    best_ler = float("inf")
+    best_epoch = 0
     num_epochs = 10
     for epoch in range(num_epochs):
         epoch_start = time.time()
@@ -137,6 +139,10 @@ def main():
             f"val_loss = {avg_val_loss:.4e} | "
             f"val_LER = {val_ler:.4e}"
         )
+        if val_ler < best_ler:
+            best_ler = val_ler
+            best_epoch = epoch
+            torch.save(model.state_dict(), "best_model.pth")
     
     total_time = time.time() - total_start
     print("\n" + "=" * 75)
@@ -144,7 +150,76 @@ def main():
     print(f"⏳ Total training time: {format_seconds(total_time)} "
           f"({total_time:.2f} seconds)")
     print("=" * 75)
+    return best_ler, best_epoch
 
+def predict(data, logs, measurement=False):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+    
+    rounds = 5  # MUST match your Stim gen_dem() parameter
+    
+    full_dataset = SyndromeDataset(data, logs, distance=3, rounds=rounds, measurement=measurement)
+    # 3. Build model
+    model = QECAlphaTransformer(
+        num_stab=full_dataset.num_stab_per_round,
+        num_cycles=full_dataset.num_cycles,
+        distance=3,
+        d_model=256,
+        nhead=4,
+        num_transformer_layers=3,
+    ).to(device)
+    
+    criterion = torch.nn.BCEWithLogitsLoss()
+    
+    model.load_state_dict(torch.load("best_model.pth", map_location=device))
+    model.to(device)
+    model.eval()
+    
+    test_loader = DataLoader(full_dataset, batch_size=256, shuffle=True)
+    # 3. Load data
+    model.eval()
+    pred_loss = 0.0
+    all_logits = []
+    all_labels = []
+
+    with torch.no_grad():
+        for batch in test_loader:
+            synd = batch["syndrome"].to(device)
+            label = batch["label"].to(device)
+            stab_id = batch["stab_id"].to(device)
+            cycle_id = batch["cycle_id"].to(device)
+
+            logits = model(synd, stab_id, cycle_id)
+            loss = criterion(logits, label)
+            pred_loss += loss.item() * synd.size(0)
+
+            all_logits.append(logits.cpu())
+            all_labels.append(label.cpu())
+
+    
+    pred_logits = torch.cat(all_logits, dim=0)
+    pred_labels = torch.cat(all_labels, dim=0)
+    pred_ler = compute_ler_from_logits(pred_logits, pred_labels)
+    return pred_loss, pred_ler
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+
+    # 1. Load Stim-generated dataset
+    dets = np.loadtxt("stim_files/data/detector_samples.csv",
+                      delimiter=",", dtype=np.uint8)
+    
+    soft_measurements = np.loadtxt("stim_files/data/gausian_soft_mesurements.csv",
+                      delimiter=",", dtype=np.float32)
+    logs = np.loadtxt("stim_files/data/logical_labels.csv",
+                      delimiter=",", dtype=np.uint8)
+    
+    
+    train(soft_measurements, logs, measurement=True)
+    pred_loss, pred_ler = predict(soft_measurements, logs, measurement=True)
+    print(f"Predicted Loss: {pred_loss:.4e} | Predicted LER: {pred_ler:.4e}")
+    
 
 if __name__ == "__main__":
     main()
