d3ff6ef1e70c1fea21b1db64da41eb06b367b832
Arjun <arjuns8@illinois.edu>
2026-03-04 12:31:02 -0500

    added neural network details and links

--- streamlit/pages/5_Links.py
+++ streamlit/pages/5_Links.py
@@ -7,3 +7,7 @@ url = "https://arxiv.org/abs/1706.03762"
 st.write("[Attention is All You Need](%s) by Vaswani et al" % url)
 url = "https://q-ctrl.com/topics/what-is-quantum-error-correction"
 st.write("[Basics of QEC](%s) by Q_CTRL" % url)
+url = "https://www.nature.com/articles/s41586-024-08148-8"
+st.write("[AlphaQubit](%s)" % url)
+url="https://doi.org/10.1103/RevModPhys.87.307"
+st.write("[Quantum Error Correction Review](%s)" % url)
