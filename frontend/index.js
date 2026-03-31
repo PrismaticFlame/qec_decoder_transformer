a3a6589eb6895b22b93d731c23ae7bc5fe946f2d
AlphaMitch <yellowyarn2003@gmail.com>
2026-03-31 12:02:03 -0400

    basic dockerfile added

---
+++ frontend/index.js
@@ -0,0 +1,15 @@
+var testVal = false
+function changeSpan() {
+    if (testVal) {
+        document.getElementById("test").innerHTML = "False"
+        testVal = false
+    } else {
+        document.getElementById("test").innerHTML = "True"
+        testVal = true
+    }
+}
+
+console.log(testVal)
+console.log("updated")
+
+// API call to get data based on button
